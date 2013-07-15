a81c3a3efce2cd17506c986a580dd985c27e9c59
Hsin-Hsiang Huang <katehuang0320@gmail.com>
2013-07-15 18:01:53 +0800

    fix a indentation

--- lib/getme.rb
+++ lib/getme.rb
@@ -11,12 +11,12 @@ module Getme
     return "What do you want me to download for you?" if what.nil?
     what = what.to_sym
     type ||= if Getme::Vendors[:files].has_key? what
-             :files
-           elsif Getme::Vendors[:zips].has_key? what
-             :zips
-           else
-             :not_available
-           end
+               :files
+             elsif Getme::Vendors[:zips].has_key? what
+               :zips
+             else
+               :not_available
+             end
     return "Did not find a candidate of #{what}, typo?" if type == :not_available
     unless Getme::Vendors[type][what].nil?
       util = Utilities.new
